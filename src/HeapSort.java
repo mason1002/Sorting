cb4995d40447975b03aa142fa72ef3a51ade88a0
mason <ms.catmissyou@gmail.com>
2018-11-22 16:50:34 +0800

    heapsort complete

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,10 +1,81 @@
+import java.util.Arrays;
+
 public class HeapSort {
 
+    public static void heapSort(int[] data, boolean reverse) {
+        if (data.length == 1) {
+            return;
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            buildHeap(data, 0, data.length - 1 - i, reverse);
+            int tmp = data[0];
+            data[0] = data[data.length - 1 - i];
+            data[data.length - 1 - i] = tmp;
+        }
+
+        System.out.println(Arrays.toString(data));
+
+    }
+
+    private static void buildHeap(int[] data, int beginIndex, int endIndex, boolean reverse) {
+
+        if (beginIndex > endIndex) {
+            return;
+        }
+        for (int i = (endIndex + beginIndex - 1) / 2; i >= beginIndex; i--) {
+            int cur = i;
+            if (reverse) {
+                // 发生交换之后需要检查孙子节点、重孙子。。
+                while (2 * cur + 1 <= endIndex) {
+                    int biggerChildIndex = 2 * cur + 1;
+                    if (biggerChildIndex + 1 <= endIndex) {
+                        if (data[biggerChildIndex] < data[biggerChildIndex + 1]) {
+                            biggerChildIndex += 1;
+                        }
+                    }
+                    //找到最大子节点，如果比当前节点大，就交换
+                    if (data[i] < data[biggerChildIndex]) {
+                        int tmp = data[i];
+                        data[i] = data[biggerChildIndex];
+                        data[biggerChildIndex] = tmp;
+                        //准备检查孙子节点
+                        cur = biggerChildIndex;
+
+                    } else {
+                        break;
+                    }
+
+                }
+            } else {
+                //小顶堆，用来从大到小排序
+                while (2 * cur + 1 <= endIndex) {
+                    int smallerChildIndex = 2 * i + 1;
+                    if (smallerChildIndex + 1 <= endIndex) {
+                        if (data[smallerChildIndex] > data[smallerChildIndex] + 1) {
+                            smallerChildIndex += 1;
+                        }
+                    }
+                    //找到最小子节点，如果比当前节点小，就交换
+                    if (data[i] > data[smallerChildIndex]) {
+                        int tmp = data[i];
+                        data[i] = data[smallerChildIndex];
+                        data[smallerChildIndex] = tmp;
+                        cur = smallerChildIndex;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+//        System.out.println(Arrays.toString(data));
+    }
 
 
+    public static void main(String[] args) {
 
-
-    public static void main(String[] args){
+        heapSort(new int[]{2, 3, 5, 6, 73, 4, 5, 52, 51, 134, 562, 6, 3, 23, 5, 253, 14}, true);
 
     }
 }
